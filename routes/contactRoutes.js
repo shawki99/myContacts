a01c4b02b08c7992423960ef34732a5ce003fe8e
shawki99 <shawkighorab@gmail.com>
2024-04-09 03:05:11 +0200

    optimizing controllers

--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -8,14 +8,14 @@ const {
     deleteContact,
 } = require("../controllers/contactController");
 
-router.route("/").get(getContacts);
+router.route("/").get(getContacts).post(createContact);
 
-router.route("/").post(createContact);
+// router.route("/").post(createContact);  NOTE -> since it is the same as the previous we can add just .post() after the .get()
 
-router.route("/:id").get(getContact);
+router.route("/:id").get(getContact).put(updateContact).delete(deleteContact);
 
-router.route("/:id").put(updateContact);
+// router.route("/:id").put(updateContact);
 
-router.route("/:id").delete(deleteContact);
+// router.route("/:id").delete(deleteContact);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app =express();
 
 const port =process .env.PORT || 5000;
 
-app.use("/api/contacs", require("./routes/contactRoutes"));
+app.use("/api/contacts", require("./routes/contactRoutes"));
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`)
