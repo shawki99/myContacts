cec37e0fc3369a44e19a5b0f7bcfde19669aba12
shawki99 <shawkighorab@gmail.com>
2024-05-06 18:23:30 +0300

    CRUD update

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -45,7 +45,19 @@ const getContact = asyncHandler(async(req,res) => {
 //@route PUT /api/contacts/:id
 //access public
 const updateContact = asyncHandler(async(req,res) => {
-    res.status(200).json({messege:`Update Contact for ${req.params.id}`});  //NOTE-> we use this to retrieve it as json data with status code
+    const contact = await Contact.findById(req.params.id);
+    if(!contact)
+        {
+            res.status(404);
+            throw new Error("Contact not found")
+        }
+
+    const updatedContact = await Contact.findByIdAndUpdate(
+        req.params.id,
+        req.body,
+        {new: true}
+    );
+    res.status(200).json(updatedContact);  //NOTE-> we use this to retrieve it as json data with status code
 });
 
 //@desc Delete contact
