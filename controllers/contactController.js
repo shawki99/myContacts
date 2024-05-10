d9386cbfc0128ee7221dea70899bf2e64357824d
shawki99 <shawkighorab@gmail.com>
2024-05-11 01:38:25 +0300

    user registration

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -3,7 +3,7 @@ const Contact = require("../models/contactModel");
 
 //@desc Get all contacts
 //@route GET /api/contacts
-//access public
+//@access public
 const getContacts = asyncHandler(async (req,res) => {
     const contacts = await Contact.find();
     // res.send("Get all contacs");//NOTE-> we use this to retrieve it as normal data
@@ -12,7 +12,7 @@ const getContacts = asyncHandler(async (req,res) => {
 
 //@desc Create new contact
 //@route POST /api/contacts
-//access public
+//@access public
 const createContact = asyncHandler(async(req,res) => {
     console.log("the request body is: ",req.body);
     const {name, email, phone} = req.body;
@@ -30,7 +30,7 @@ const createContact = asyncHandler(async(req,res) => {
 
 //@desc Get  contact
 //@route GET /api/contacts/:id
-//access public
+//@access public
 const getContact = asyncHandler(async(req,res) => {
     const contact = await Contact.findById(req.params.id);
     if(!contact)
@@ -43,7 +43,7 @@ const getContact = asyncHandler(async(req,res) => {
 
 //@desc Update contact
 //@route PUT /api/contacts/:id
-//access public
+//@access public
 const updateContact = asyncHandler(async(req,res) => {
     const contact = await Contact.findById(req.params.id);
     if(!contact)
@@ -62,7 +62,7 @@ const updateContact = asyncHandler(async(req,res) => {
 
 //@desc Delete contact
 //@route DELETE /api/contacts/:id
-//access public
+//@access public
 const deleteContact = asyncHandler(async(req,res) => {
     const contact = await Contact.findById(req.params.id);
     if(!contact)
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,10 +1,29 @@
 const asyncHandler = require("express-async-handler");
-
+const bcrypt = require("bcrypt");
+const User = require("../models/userModel")
 
 //@desc Resgister a user
 //@route POST /api/users/register
 //@access public
 const registerUser = asyncHandler(async (req,res) =>{
+    const {username, email, password} = req.body;
+    if(!username || !email || !password){
+        res.status(400);
+        throw new Error("ALL fields are mandatory");
+    }
+    const userAvailable = await User.findOne({email});
+    if(userAvailable){
+        res.status(400);
+        throw new Error("Email is already registered");
+    }
+
+// Hash password
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = await User.create({
+        username,
+        email,
+        password: hashedPassword,
+    })
     res.json({message: "registered the user"});
 });
 
